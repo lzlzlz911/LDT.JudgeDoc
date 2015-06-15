d985c314bc0f410c83ec1ce8c8b6caddce291c45
lzlzlz911 <lzlzlz911@hotmail.com>
2015-06-15 17:39:14 +0800

    aa

---
+++ IDT.JudgeDoc.DAL.Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDT.JudgeDoc.DAL.Test {
+    class Program {
+        static void Main(string[] args) {
+            new UserTest().QueryUser();
+            Console.Read();
+        }
+    }
+}
